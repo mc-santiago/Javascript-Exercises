e9c48e166bc8890f99466c9def008fd62266b5ac
Antonio Esquembre <antonio@wildcodeschool.es>
2019-05-28 05:27:10 -0400

    async

---
+++ async/main.js
@@ -0,0 +1,11 @@
+// Carga el fichero config.json y busca el dataFilePath
+// Carga el fichero ubicado en dataFilePath
+// Imprime en la consola los usuarios mayores de 25 cuyo nombre empiece por la letra A (utiliza map, filter o reduce)
+// Don't repeat yourself
+
+
+// Version promesas (usa then)
+
+
+
+// Version async/await
